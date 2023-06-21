7c4c1394aead4a369b9ad1784103571c6f178879
EdwinAhenkan <edwinahenkan@gmail.com>
2023-06-21 09:20:55 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,19 +11,17 @@ int main(void)
 	unsigned long fib1 = 0, fib2 = 1, fibsum;
 	float tot_sum;
 
-	while(1)
+	while (1)
 	{
 		fibsum = fib1 + fib2;
 		if (fibsum > 4000000)
 			break;
-		
 		if ((fibsum % 2) == 0)
-			tot_sum +=fibsum;
+			tot_sum += fibsum;
 
 		fib1 = fib2;
 		fib2 = fibsum;
 	}
 	printf("%.0f\n", tot_sum);
-	
 	return (0);
 }
